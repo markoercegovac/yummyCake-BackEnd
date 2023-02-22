f8f6ede1b30b52075e08fd61a1cf576ae9ef495a
markoercegovac <ercegovacmarko@yahoo.com>
2023-02-22 15:41:23 -0500

    Initial commit

---
+++ yummyCake/src/main/java/yummy/cake/com/YummyCakeApplication.java
@@ -0,0 +1,13 @@
+package yummy.cake.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyCakeApplication.class, args);
+	}
+
+}
---
+++ yummyCake/src/test/java/yummy/cake/com/YummyCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package yummy.cake.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
